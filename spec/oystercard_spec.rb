e739684bac5418444cede26c1dc47d9098fa6b14
phoebe <phoebe.greig@gmail.com>
2016-03-07 15:53:05 +0000

    top up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,7 +2,14 @@ require 'oystercard'
 
 describe Oystercard do
   it { is_expected.to respond_to(:balance) }
+
+  it { is_expected.to respond_to(:top_up) }
+
   it "should have a default balance of 0" do
     expect(subject.balance).to eq 0
+  end
+
+  it "should increase the balance by £10" do
+  	expect{subject.top_up(10)}.to change {subject.balance}.by(10)
   end
 end
