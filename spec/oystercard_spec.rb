c414673efc726ac7bf544638637524f0b503dfc6
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-07 15:43:11 +0000

    Created balance var

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  it { is_expected.to respond_to(:balance) }
+  it "should have a default balance of 0" do
+    expect(subject.balance).to eq 0
+  end
 end
