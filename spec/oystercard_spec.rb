9acbede264d4375b603367911d7dbcd99bf2cd5a
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-07 16:28:19 +0000

    Added touch_in, touch_out, in_journey? Refactored.

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,26 +4,42 @@ describe Oystercard do
 
   subject(:oystercard) { described_class.new }
 
-  it { is_expected.to respond_to(:balance) }
-
-  it { is_expected.to respond_to(:top_up) }
-
-  it "should have a default balance of 0" do
-    expect(subject.balance).to eq 0
+  describe "#balance" do
+    it "should have a default balance of 0" do
+      expect(subject.balance).to eq 0
+    end
   end
 
-  it "should increase the balance by £10" do
-  	expect{subject.top_up(10)}.to change {subject.balance}.by(10)
+  describe "#top_up" do
+    it "should increase the balance by £10" do
+    	expect{subject.top_up(10)}.to change {subject.balance}.by(10)
+    end
+
+    it "should raise error when balance is above maximum limit" do
+      amount = described_class::MAXIMUM_BAL + 1
+      message = "Your balance cannot exceed #{described_class::MAXIMUM_BAL}."
+      expect{ subject.top_up(amount) }.to raise_error message
+    end
   end
 
-  it "should raise error when balance is above maximum limit" do
-    amount = described_class::MAXIMUM_BAL + 1
-    message = "Your balance cannot exceed #{described_class::MAXIMUM_BAL}."
-    expect{ subject.top_up(amount) }.to raise_error message
+  describe "#reduce" do
+    it "should reduce the balance by £10" do
+    	o = Oystercard.new(20)
+    	expect{o.deduct(10)}.to change {o.balance}.by(-10)
+    end
   end
 
-  it "should reduce the balance by £10" do
-  	o = Oystercard.new(20)
-  	expect{o.deduct(10)}.to change {o.balance}.by(-10)
+  describe "#touch_in" do
+    it "should change travelling status" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+  end
+
+  describe "#touch_out" do
+    it "should change travelling status" do
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
   end
 end
