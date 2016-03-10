bd86a5fab5c14fc895d7208e300e26302ebd9405
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-10 21:44:04 +0000

    Refactored Station spec

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,17 +1,17 @@
 require 'station'
 
 describe Station do
-  subject {described_class.new("Aldgate", 1)}
+  subject(:station) { described_class.new("Aldgate", 1) }
 
   describe '#name' do
     it 'starts with a name' do
-      expect(subject.name).to eq 'Aldgate'
+      expect(station.name).to eq 'Aldgate'
     end
   end
 
   describe '#zone' do
     it 'starts with a zone' do
-      expect(subject.zone).to eq 1
+      expect(station.zone).to eq 1
     end
   end
 end
